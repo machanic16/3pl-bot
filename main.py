4a166488693494120f0d419e8071eae336ff2127
Joel Tovar <jatr160397@gmail.com>
2021-01-12 19:57:51 -0500

    the bot is alive, now I wil find out how put buttons to interact with the custumers more easily

--- main.py
+++ main.py
@@ -1,80 +1,30 @@
 # -*- coding: UTF8 -*-
 import requests
-import datetime
+import json
+import configparser as cfg
 
 
+class telegram_chatbot():
 
-class BotHandler:
-    def __init__(self, token):
-            self.token = token
-            self.api_url = "https://api.telegram.org/bot{}/".format(token)
+    def __init__(self, config):
+        self.token = self.read_token_from_config_file(config)
+        self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
-    #url = "https://api.telegram.org/bot<token>/"
+    def get_updates(self, offset=None):
+        url = self.base + "getUpdates?timeout=100"
+        if offset:
+            url = url + "&offset={}".format(offset + 1)
+        r = requests.get(url)
+        return json.loads(r.content)
 
-    def get_updates(self, offset=0, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
+    def send_message(self, msg, chat_id):
+        url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
+        if msg is not None:
+            requests.get(url)
 
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text, 'parse_mode': 'HTML'}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
+    def read_token_from_config_file(self, config):
+        parser = cfg.ConfigParser()
+        parser.read(config)
+        return parser.get('creds', 'token')
 
-    def get_first_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[0]
-        else:
-            last_update = None
-
-        return last_update
-
-
-token = '1557905827:AAGOIrHtbYggDQDLCUeaSUWXY08jho8sw28' #Token of your bot
-CustomerService3pl24Bot = BotHandler(token) #Your bot's name
-
-
-
-def main():
-    new_offset = 0
-    print('hi, now launching...')
-
-    while True:
-        all_updates=CustomerService3pl24Bot.get_updates(new_offset)
-
-        if len(all_updates) > 0:
-            for current_update in all_updates:
-                print(current_update)
-                first_update_id = current_update['update_id']
-                if 'text' not in current_update['message']:
-                    first_chat_text='New member'
-                else:
-                    first_chat_text = current_update['message']['text']
-                first_chat_id = current_update['message']['chat']['id']
-                if 'first_name' in current_update['message']:
-                    first_chat_name = current_update['message']['chat']['first_name']
-                elif 'new_chat_member' in current_update['message']:
-                    first_chat_name = current_update['message']['new_chat_member']['username']
-                elif 'from' in current_update['message']:
-                    first_chat_name = current_update['message']['from']['first_name']
-                else:
-                    first_chat_name = "unknown"
-
-                if first_chat_text == 'Hi':
-                    CustomerService3pl24Bot.send_message(first_chat_id, 'Morning ' + first_chat_name)
-                    new_offset = first_update_id + 1
-                else:
-                    CustomerService3pl24Bot.send_message(first_chat_id, 'How are you doing '+first_chat_name)
-                    new_offset = first_update_id + 1
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+        
